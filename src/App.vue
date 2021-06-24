aea6194c6f4bbfbdb39120f85421e75ec74210ee
wporta06 <portawalid@gmail.com>
2021-06-24 17:12:40 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,77 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h3>{{title}}</h3>
+    <Tasks v-bind:tasks="tasks" v-on:completeTaskEvent="completeTask" v-on:deleteTaskEvent="deleteTask"></Tasks>
+    <input  class="form-control mb-2" @keyup.enter="addNewTask" type="text" v-model="newTask" >
+    <button  class="btn btn-success btn-block" v-on:click="addNewTask">Add new Task</button>
   </div>
 </template>
 
+
+<script>
+
+import Tasks from "@/components/Tasks";
+
+export default {
+  name: 'app',
+  components: {
+    Tasks
+  },
+  data(){
+    return{
+      title:"All Task ",
+      newTask: null,
+      tasks:[{
+        body:"Go to shop1",
+        completed: false
+      },
+      {
+        body:"Go to shop2",
+        completed: false
+      },
+      {
+        body:"Go to shop3",
+        completed: false
+      },
+      {
+        body:"Go to shop4",
+        completed: false
+      },
+      ]
+    }
+  },
+  methods: {
+    addNewTask(){
+        console.log("addddd");
+      this.tasks.push({     //add task to tasks
+        body: this.newTask,
+        completed: false
+      });
+      this.newTask= null;   //to empty input after add task
+    },
+    completeTask(task){
+       console.log("done"+task);
+      task.completed= !task.completed;
+    },
+    deleteTask(index){
+        console.log("delete task " +index);
+      this.tasks.splice(index, 1);
+    }
+
+  }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+#app{
+  margin-top:60px;
+
+}
+.card{
+  padding: 20px;
+  background: #eee;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="card mb-1 text-left">
+      <ul class="list-group  ">
+        <li class="list-group-item shadow p-3 mb-5 bg-light rounded " v-for="(task,index) in tasks" :key="index">
+          <div :class="task.completed ? 'completed' : ''">{{task.body}}</div>
+
+          <!-- for delete task & complet tasks   -->
+          <div class="icons">
+            <i class="fas fa-check-circle " @click.prevent="completeTask(task)"></i>
+            <i class="fas fa-trash-alt ml-3" @click.prevent="deleteTask(index)"></i>
+          </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: null
+  },
+  methods: {
+    completeTask(task){
+      this.$emit("completeTaskEvent", task); //send event and where is it
+    },
+    deleteTask(index){
+      this.$emit("deleteTaskEvent", index)
+    }
+  }
+
+};
+</script>
+
+<style scoped>
+.completed{
+  color: rgb(255, 0, 0);
+  justify-content: space-between;
+  
+}
+.list-group-item{
+  display:flex;
+  justify-content: space-between;
+}
+</style>
+
+
+
